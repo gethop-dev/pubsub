4679a93ccec59c8c3e8ce9a1ab5d269ec9079dcf
Iñaki Arenaza <inaki.arenaza@magnet.coop>
2020-05-02 01:28:43 +0200

    Bump dependencies to newer versions

---
+++ CHANGELOG.md
@@ -0,0 +1,30 @@
+# Changelog
+All notable changes to this project will be documented in this file.
+
+The format is based on [Keep a Changelog](http://keepachangelog.com/en/1.0.0/).
+
+## [Unreleased]
+### Changed
+- Bumped dependencies to newer versions
+
+### Added
+- Added this changelog
+
+## [0.3.1] - 2019-02-26
+### Changed
+- Bumped minimum Leiningen version to 2.9.0
+- Reorganized dev profile definition
+- Fixed typos in README.md and added more examples
+
+### Added
+- Add Travis CI integration
+- Add deployment configuration and integration tests CI 
+
+## [0.3.0] - 2019-02-26
+- Initial commit (previous versions were not publicly released)
+
+[UNRELEASED]:  https://github.com/magnetcoop/scheduling.twarc/compare/v0.3.1...HEAD
+[0.3.1]: https://github.com/magnetcoop/scheduling.twarc/releases/tag/v0.3.1
+[0.3.0]: https://github.com/magnetcoop/scheduling.twarc/releases/tag/v0.3.0
+
+
--- project.clj
+++ project.clj
@@ -7,11 +7,11 @@
   :dependencies [[org.clojure/clojure "1.10.0"]
                  [clojurewerkz/machine_head "1.0.0"]
                  [com.novemberain/langohr "5.1.0"]
-                 [diehard "0.7.2"]
+                 [diehard "0.9.4"]
                  [duct/logger "0.3.0"]
-                 [integrant "0.7.0"]
-                 [org.bouncycastle/bcprov-jdk15on "1.60"]
-                 [org.bouncycastle/bcpkix-jdk15on "1.60"]]
+                 [integrant "0.8.0"]
+                 [org.bouncycastle/bcprov-jdk15on "1.65"]
+                 [org.bouncycastle/bcpkix-jdk15on "1.65"]]
   :deploy-repositories [["snapshots" {:url "https://clojars.org/repo"
                                       :username :env/clojars_username
                                       :password :env/clojars_password
@@ -25,6 +25,6 @@
    :repl {:repl-options {:host "0.0.0.0"
                          :port 4001}}
    :profiles/dev {}
-   :project/dev {:plugins [[jonase/eastwood "0.3.4"]
-                           [lein-cljfmt "0.6.2"]]
+   :project/dev {:plugins [[jonase/eastwood "0.3.11"]
+                           [lein-cljfmt "0.6.7"]]
                  :dependencies [[com.taoensso/nippy "2.14.0"]]}})
