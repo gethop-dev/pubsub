d85cde0f7f590f8cac12a76bad7074e7e6567be6
Iñaki Arenaza <inaki.arenaza@magnet.coop>
2023-03-12 15:57:57 +0100

    Prepare for next development release.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject dev.gethop/pubsub "0.4.0"
+(defproject dev.gethop/pubsub "0.4.1-SNAPSHOT"
   :description "Duct + Integrant library wrapping MQTT & AQMP publish/subcribe clients with a common boundary protocol"
   :url "https://github.com/gethop-dev/pubsub"
   :license {:name "Mozilla Public Licence 2.0"
